b2aa8c3f23242d527e0a8b71ffc40eef0d6160e6
Christopher Wilkinson <christopher@cw1998.uk>
2020-02-02 02:48:46 +0000

    Fix a broken link and add breadcrumbs to county pages

--- generate_county_collection.sh
+++ generate_county_collection.sh
@@ -9,6 +9,13 @@ cat > _counties/${county,,}.md << EOF
 layout: county
 county: $county
 title: Co. $county
+breadcrumbs:
+  - text: Home
+    link: /
+  - text: Resources
+    link: /resources.html
+  - text: County Townland Geometry Datasets
+    link: /counties
 ---
 EOF
 done < <(tail -n +2 _data/counties.yaml)
